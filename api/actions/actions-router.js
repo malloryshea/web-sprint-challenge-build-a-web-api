170c595ec2a1888ad2c2a1768132267b618cb9f9
Mallory Shea <malloryshea@Mallorys-MacBook-Air.local>
2022-01-14 13:07:30 -0600

    setup for endpoints

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,37 @@
-// Write your "actions" router here!
+const express = require('express');
+
+const router = express.Router();
+
+const Actions = require('./actions-model');
+
+router.get('/', (req,res) => {
+    res.json({
+        message: "get all -actions"
+    })
+})
+
+router.get('/:id', (req,res) => {
+    res.json({
+        message: "get by id -actions"
+    })
+})
+
+router.post('/', (req,res) => {
+    res.json({
+        message: "create new -actions"
+    })
+})
+
+router.put('/:id', (req,res) => {
+    res.json({
+        message: "update -actions"
+    })
+})
+
+router.delete('/:id', (req,res) => {
+    res.json({
+        message: "delete -actions"
+    })
+})
+
+module.exports = router;// Write your "actions" router here!
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -7,7 +7,37 @@ const Projects = require('./projects-model');
 
 router.get('/', (req,res) => {
     res.json({
-        message: "Good boy"
+        message: "get all -projects"
+    })
+})
+
+router.get('/:id', (req,res) => {
+    res.json({
+        message: "get by id -projects"
+    })
+})
+
+router.get('/:id/actions', (req,res) => {
+    res.json({
+        message: "get actions by id -projects"
+    })
+})
+
+router.post('/', (req,res) => {
+    res.json({
+        message: "create new -projects"
+    })
+})
+
+router.put('/:id', (req,res) => {
+    res.json({
+        message: "update -projects"
+    })
+})
+
+router.delete('/:id', (req,res) => {
+    res.json({
+        message: "delete -projects"
     })
 })
 
